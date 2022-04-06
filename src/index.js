b658626cae14eea8c55083d5c9c8cc66049ce4f1
kamkow1 <ikamileq@gmail.com>
2022-04-06 10:59:02 -0700

    fs support

--- src/index.js
+++ src/index.js
@@ -2,9 +2,17 @@ import { parser } from "./parser.js";
 import { transpiler } from "./transpiler.js";
 import { lexer } from "./lexer.js";
 
-const program = 'divide 3 2';
+import fs from 'fs';
+import { program } from "commander";
 
-const transpiledProgram = transpiler(parser(lexer(program)));
+program.option('-p', '--path <dir>').parse(process.argv);
+
+const path = program.path;
+const code = fs.readFileSync(path).toString('ascii');
+
+//const program = 'divide 3 2';
+
+const transpiledProgram = transpiler(parser(lexer(code)));
 const result = eval(transpiledProgram);
 
 console.info(transpiledProgram);
