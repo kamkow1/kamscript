94eff4b71fab3272cf943636fbf99b40532d05b1
kamkow1 <ikamileq@gmail.com>
2022-04-06 10:17:17 -0700

    transpiler

---
+++ src/transpiler.js
@@ -0,0 +1,16 @@
+const transpiler = ast => {
+    const opMap = { 
+        sum: '+', 
+        mul: '*', 
+        sub: '-', 
+        div: '/' 
+    };
+
+    const transpileNode = ast => ast.type === Num ? transpileNum(ast) : transpileOp(ast);
+
+    const transpileNum = ast => ast.val;
+
+    const transpileOp = ast => `(${ast.expr.map(transpileNode).join(' ' + opMap[ast.val] + ' ')})`;
+    
+    return transpileNode(ast);
+  };
