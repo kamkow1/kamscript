d8deec12eb16cf85ecbdc1dd337a60e758cc9b3b
kamkow1 <ikamileq@gmail.com>
2022-04-06 10:46:04 -0700

    changed operator names

--- src/index.js
+++ src/index.js
@@ -2,9 +2,10 @@ import { parser } from "./parser.js";
 import { transpiler } from "./transpiler.js";
 import { lexer } from "./lexer.js";
 
-const program = 'mul 3 2';
+const program = 'divide 3 2';
 
-let transpiledProgram = transpiler(parser(lexer(program)));
-console.log(transpiledProgram);
-console.log(lexer(program));
-console.log(parser(lexer(program)));
+const transpiledProgram = transpiler(parser(lexer(program)));
+const result = eval(transpiledProgram);
+
+console.info(transpiledProgram);
+console.info(result);
--- src/transpiler.js
+++ src/transpiler.js
@@ -1,29 +1,20 @@
 import { Num } from "./parser.js";
-/*import { sum } from './functions.js';
-import { sub } from "./functions.js";
-import { div } from "./functions.js";
-import { mul } from "./functions.js";*/
-
-export const sum = (...operands) => operands.reduce((acc, curr) => acc + curr);
-
-export const sub = (...operands) => operands.reduce((acc, curr) => acc - curr);
-
-export const mul = (...operands) => operands.reduce((acc, curr) => acc * curr, 1);
-
-export const div = (...operands) => operands.reduce((acc, curr) => acc / curr, 0);
+import { add } from './functions.js';
+import { subtract } from "./functions.js";
+import { divide } from "./functions.js";
+import { multiply } from "./functions.js";
 
 export const transpiler = abstractSyntaxTree => {
     const opMap = { 
-        sum: '+', 
-        mul: '*', 
-        sub: '-', 
-        div: '/' 
+        add: '+', 
+        multiply: '*', 
+        subtract: '-', 
+        divide: '/' 
     };
 
     const transpileNode = abstractSyntaxTree => abstractSyntaxTree.type === Num ? transpileNum(abstractSyntaxTree) : transpileOp(abstractSyntaxTree);
 
     const transpileNum = abstractSyntaxTree => abstractSyntaxTree.val;
-    console.log(abstractSyntaxTree.val + ' !!!');
 
     const transpileOp = abstractSyntaxTree => `(${abstractSyntaxTree.expr.map(transpileNode).join(' ' + opMap[abstractSyntaxTree.val] + ' ')})`;
 
